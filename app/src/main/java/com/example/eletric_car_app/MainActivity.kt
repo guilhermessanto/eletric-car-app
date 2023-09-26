1d97789e07eb2a9defa2a7b6047c689897456a47
guilherme <g.santossantana22@gmail.com>
2023-09-26 10:29:16 -0300

    Inicio do projeto Eletric car app

---
+++ app/src/main/java/com/example/eletric_car_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eletric_car_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
