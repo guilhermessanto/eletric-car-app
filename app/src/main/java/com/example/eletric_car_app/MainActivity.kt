70a029eb42af70642774f206faecfe21b8939f29
guilherme <g.santossantana22@gmail.com>
2023-09-28 18:16:53 -0300

    capturando-valores
    capturando valores dos inputs de km e preco

--- app/src/main/java/com/example/eletric_car_app/MainActivity.kt
+++ app/src/main/java/com/example/eletric_car_app/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.eletric_car_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var preco: EditText
+    lateinit var kmPercorrido: EditText
+    lateinit var  btnCalcular: Button
+    lateinit var resultado: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+        setupListeners()
+    }
+    fun setupView(){
+        preco = findViewById(R.id.et_preco_kwh)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        resultado = findViewById(R.id.tv_resultado)
+        btnCalcular = findViewById(R.id.btn_calcular)
+    }
+    fun setupListeners(){
+        btnCalcular.setOnClickListener{
+            calcular()
+        }
+    }
+    fun calcular(){
+        val preco=preco.text.toString().toFloat()
+        val km = kmPercorrido.text.toString().toFloat()
+        val result = preco/km
+        resultado.text = result.toString()
+
     }
 }
