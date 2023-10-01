b23b32a6ef68d02577919c9ff4bf534d58dd1bed
guilherme <g.santossantana22@gmail.com>
2023-09-30 21:06:32 -0300

    http-load-no-conection

--- app/src/main/java/com/example/eletric_car_app/ui/CarFragment.kt
+++ app/src/main/java/com/example/eletric_car_app/ui/CarFragment.kt
@@ -1,19 +1,30 @@
 package com.example.eletric_car_app.ui
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eletric_car_app.R
 import com.example.eletric_car_app.data.CarFactory
+import com.example.eletric_car_app.domain.Carro
 import com.example.eletric_car_app.ui.Adapter.CarAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.json.JSONArray
 import org.json.JSONObject
+import org.json.JSONTokener
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
@@ -23,6 +34,11 @@ import java.net.URL
 class CarFragment : Fragment() {
     lateinit var fabCalcular: FloatingActionButton
     lateinit var listaCarros: RecyclerView
+    lateinit var progress: ProgressBar
+    lateinit var noInternetImage: ImageView
+    lateinit var noInternetText: TextView
+
+    var carrosArray: ArrayList<Carro> = ArrayList()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -32,17 +48,40 @@ class CarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupView(view)
-        setupList()
         setupListeners()
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (checkForInternet(context)) {
+            callService()
+        } else {
+            emptyState()
+        }
+    }
+
+    fun emptyState() {
+        progress.isVisible = false
+        listaCarros.isVisible = false
+        noInternetImage.isVisible = true
+        noInternetText.isVisible = true
     }
 
     fun setupView(view: View) {
-        fabCalcular = view.findViewById(R.id.fab_calcular)
-        listaCarros = view.findViewById(R.id.rl_lista_carros)
+        view.apply {
+            fabCalcular = findViewById(R.id.fab_calcular)
+            listaCarros = findViewById(R.id.rl_lista_carros)
+            progress = findViewById(R.id.pb_loader)
+            noInternetImage = findViewById(R.id.iv_empty_state)
+            noInternetText = findViewById(R.id.tv_no_wifi)
+        }
     }
 
     fun setupList() {
-        val adapter = CarAdapter(CarFactory.list)
+        val adapter = CarAdapter(carrosArray)
+        listaCarros.isVisible = true
         listaCarros.adapter = adapter
     }
 
@@ -51,25 +90,63 @@ class CarFragment : Fragment() {
             startActivity(Intent(context, CalculaAutonomiaActivity::class.java))
         }
 
+
     }
 
-    inner class MyTask : AsyncTask<String, String, String> {
+    fun callService() {
+        val urlBase = "https://igorbag.github.io/cars-api/cars.json"
+        MyTask().execute(urlBase)
+        progress.isVisible = true
+    }
+
+    fun checkForInternet(context: Context?): Boolean {
+        val connectivityManager =
+            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
+
+    }
+
+    inner class MyTask : AsyncTask<String, String, String>() {
         override fun onPreExecute() {
             super.onPreExecute()
             Log.d("MyTask", "Iniciando...")
         }
 
         override fun doInBackground(vararg params: String?): String {
-            TODO("Not yet implemented")
             var urlConnection: HttpURLConnection? = null
             try {
                 val urlBase = URL(params[0])
                 urlConnection = urlBase.openConnection() as HttpURLConnection
                 urlConnection.connectTimeout = 60000
                 urlConnection.readTimeout = 60000
+                urlConnection.setRequestProperty(
+                    "Accept",
+                    "application/json"
+                )
+                val responseCode = urlConnection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
 
-                var inString = streamToString(urlConnection.inputStream)
-                publishProgress(inString)
+                    var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
+                    publishProgress(response)
+                } else {
+                    Log.e("Error", "Servico indisponivel no momento")
+
+                }
+
             } catch (ex: Exception) {
                 Log.e("Error", "error ao realizar processamento...")
             } finally {
@@ -80,33 +157,47 @@ class CarFragment : Fragment() {
 
         override fun onProgressUpdate(vararg values: String?) {
             try {
+                val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
+                for (i in 0 until jsonArray.length()) {
+                    val id = jsonArray.getJSONObject(i).getString("id")
+                    Log.d("ID ->", id)
 
-                var json: JSONObject
-                values[0]?.let {
-                    json = JSONObject(it)
+                    val preco = jsonArray.getJSONObject(i).getString("preco")
+                    Log.d("preco ->", preco)
+
+                    val bateria = jsonArray.getJSONObject(i).getString("bateria")
+                    Log.d("bateria ->", bateria)
+
+                    val potencia = jsonArray.getJSONObject(i).getString("potencia")
+                    Log.d("potencia ->", potencia)
+
+                    val recarga = jsonArray.getJSONObject(i).getString("recarga")
+                    Log.d("recarga ->", recarga)
+
+                    val urlPhoto = jsonArray.getJSONObject(i).getString("urlPhoto")
+                    Log.d("urlPhoto ->", urlPhoto)
+
+                    val model = Carro(
+                        id = id.toInt(),
+                        preco = preco,
+                        bateria = bateria,
+                        potencia = potencia,
+                        recarga = recarga,
+                        urlPhoto = urlPhoto
+                    )
+                    carrosArray.add(model)
                 }
-            } catch (ex: Exception) {
+                setupList()
+                progress.isVisible = false
+                noInternetImage.isVisible = false
+                noInternetText.isVisible = false
 
+            } catch (ex: Exception) {
+                Log.e("Erro ->", ex.message.toString())
             }
         }
 
-        fun streamToString(inputString: InputStream): String {
-            val bufferReader = BufferedReader(InputStreamReader(inputString))
-            var line: String
-            var result = ""
-            try {
-                do {
-                    line = bufferReader.readLine()
-                    line?.let {
-                        result == line
-                    }
-                } while (line != null)
-            } catch (ex: Exception) {
-                Log.d("Erro", "Erro ao parcelar Stream")
-            }
-            return result
 
-        }
     }
 }
 
